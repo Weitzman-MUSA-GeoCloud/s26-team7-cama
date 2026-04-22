5ba15408ac5e4141bc118b43f190bd2a1a20d7bb
Mjumbe Poe <mjumbewu@gmail.com>
2026-04-22 11:56:27 -0400

    Clean up the property tile info query

--- tasks/run_sql/sql/derived/property_tile_info.sql
+++ tasks/run_sql/sql/derived/property_tile_info.sql
@@ -6,27 +6,20 @@ CREATE OR REPLACE VIEW `{{ project_id }}.derived.property_tile_info` AS (
       market_value,
       ROW_NUMBER() OVER (PARTITION BY property_id ORDER BY year DESC) as rn
     FROM `{{ project_id }}.core.opa_assessments`
-  ),
-  pivoted_assessments AS (
-    SELECT
-      property_id,
-      market_value AS tax_year_assessed_value,
-      year AS tax_year
-    FROM recent_assessments
-    WHERE rn = 1
   )
   SELECT
-    ST_ASGEOJSON(p.geometry) AS geometry,
-    p.property_id,
-    op.category_code_description,
-    op.year_built,
-    op.zoning,
-    a.tax_year_assessed_value,
-    a.tax_year,
-    -- ca.current_assessed_value
-  FROM `{{ project_id }}.core.pwd_parcels` p
-  JOIN `{{ project_id }}.derived.opa_residential_properties` op ON p.property_id = op.property_id
-  LEFT JOIN pivoted_assessments a ON p.property_id = a.property_id
-  -- LEFT JOIN `{{ project_id }}.derived.current_assessments` ca ON p.property_id = ca.property_id
-  WHERE p.geometry IS NOT NULL
+    ST_ASGEOJSON(parcel.geometry) AS geometry,
+    parcel.property_id,
+    property.category_code_description,
+    property.year_built,
+    property.zoning,
+    assessment.market_value AS tax_year_assessed_value,
+    assessment.year AS tax_year,
+    -- prediction.current_assessed_value
+  FROM `{{ project_id }}.core.pwd_parcels` parcel
+  JOIN `{{ project_id }}.derived.opa_residential_properties` property ON parcel.property_id = property.property_id
+  LEFT JOIN recent_assessments assessment ON parcel.property_id = assessment.property_id
+  -- LEFT JOIN `{{ project_id }}.derived.current_assessments` prediction ON parcel.property_id = prediction.property_id
+  WHERE parcel.geometry IS NOT NULL
+    AND assessment.rn = 1
 );
